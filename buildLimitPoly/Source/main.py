12a773ddfb1037e89c0e19e4cdbd4288206309c2
AnBa <acebadini@hotmail.com>
2019-02-20 15:56:33 +0100

    Added file with overlapping building_limits to test that scenario Added pdf of assignment Moved folder structure around a little

--- buildLimitPoly/Source/main.py
+++ buildLimitPoly/Source/main.py
@@ -4,15 +4,20 @@ from preproccessAPI import findBuildPolygons
 #Constants
 DEFAULT_IN_PATH = "TestDocs/example.txt"
 DEFAULT_OUT_PATH = "TestDocs/output.txt"
-UNIT_TEST_IN_PATH = "TestDocs/test.txt"
-UNIT_TEST_OUT_PATH = "TestDocs/testResult.txt"
+UNIT_TEST1_IN_PATH = "TestDocs/test.txt"
+UNIT_TEST1_OUT_PATH = "TestDocs/testResult.txt"
+TEST_OVERLAP_IN_PATH = "TestDocs/testOverlap.txt"
+TEST_OVERLAP_OUT_PATH = "TestDocs/resultsOverlap.txt"
 
 # Main declaration
 def main():
+
     findBuildPolygons(DEFAULT_IN_PATH, DEFAULT_OUT_PATH) #main call
 
     #Unit test tests handling of multiple build zones and gap in height_limit zones, output JSON file may need to be manually verified for sanity
-    findBuildPolygons(UNIT_TEST_IN_PATH, UNIT_TEST_OUT_PATH) #Unit test call
+    #findBuildPolygons(UNIT_TEST1_IN_PATH, UNIT_TEST1_OUT_PATH) #Unit test call
+    #Unit test that checks handling of overlapping building_limit zones
+    #findBuildPolygons(TEST_OVERLAP_IN_PATH, TEST_OVERLAP_OUT_PATH)
 
 if __name__ == "__main__":
     main()
